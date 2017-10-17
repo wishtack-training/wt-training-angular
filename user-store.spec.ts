7c488e0a81c49aa724d03c9c0fd82d037cd69047
Younes JAAIDI <yjaaidi@gmail.com>
2017-10-17 16:44:53 +0200

    UserStore specs.

---
+++ user-store.spec.ts
@@ -0,0 +1,49 @@
+/**
+ *
+ * (c) 2013-2017 Wishtack
+ *
+ * $Id: $
+ */
+
+import { UserStore } from './user/user-store';
+
+
+const assert = (value, expectedValue) => {
+    if (value !== expectedValue) {
+        throw new Error(`Assertion failed.
+        Value:
+        ${value}
+        Expected:
+        ${expectedValue}
+`)
+    }
+};
+
+const userStore = new UserStore();
+
+const user1 = new User('Foo', 'BAR');
+const user2 = new User('John', 'DOE');
+const user3 = new User('Foo', 'BAR');
+
+const userList1 = userStore.getUserList();
+
+userStore.addUser(user1);
+userStore.addUser(user2);
+userStore.addUser(user3);
+
+const userList2 = userStore.getUserList();
+
+userStore.removeUser(user3);
+
+const userList3 = userStore.getUserList();
+
+assert(userList1.length, 0);
+
+assert(userList2.length, 3);
+assert(userList2[0], user1);
+assert(userList2[1], user2);
+assert(userList2[2], user3);
+
+assert(userList3.length, 2);
+assert(userList3[0], user1);
+assert(userList3[1], user2);
---
+++ user/user-store.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * (c) 2013-2017 Wishtack
+ *
+ * $Id: $
+ */
+
+export class UserStore {
+
+}
