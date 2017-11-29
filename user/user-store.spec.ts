5e7ee2a2c49c02da3163a2d376840c440820c23c
Younes JAAIDI <yjaaidi@gmail.com>
2017-11-29 11:06:03 +0100

    Added UserStore.

--- user/user-store.spec.ts
+++ user/user-store.spec.ts
@@ -5,6 +5,10 @@
  * $Id: $
  */
 
+import { UserStore } from './user-store';
+import { User } from './user';
+
+
 const assert = (expected, value) => {
 
     if (expected !== value) {
@@ -26,23 +30,33 @@ const user3 = new User('Foo', 'BAR');
 
 const userList1 = userStore.getUserList();
 
-assert(0, userList1.length);
-
 userStore.addUser(user1);
 userStore.addUser(user2);
 userStore.addUser(user3);
 
 const userList2 = userStore.getUserList();
 
+userStore.removeUser(user3);
+
+const userList3 = userStore.getUserList();
+
+userStore.undo();
+userStore.undo();
+userStore.undo();
+
+const userList4 = userStore.getUserList();
+
+assert(0, userList1.length);
+
 assert(3, userList2.length);
 assert(user1, userList2[0]);
 assert(user2, userList2[1]);
 assert(user3, userList2[2]);
 
-userStore.removeUser(user3);
-
-const userList3 = userStore.getUserList();
-
 assert(2, userList3.length);
 assert(user1, userList3[0]);
 assert(user2, userList3[1]);
+
+assert(1, userList4.length);
+assert(user1, userList4[0]);
+
---
+++ user/user-store.ts
@@ -0,0 +1,37 @@
+/**
+ *
+ * (c) 2013-2017 Wishtack
+ *
+ * $Id: $
+ */
+import { User } from './user';
+
+export class UserStore {
+
+    private _userList: User[] = [];
+    private _userListHistory = [];
+
+    getUserList() {
+        return this._userList;
+    }
+
+    addUser(user: User) {
+        const userList = [...this._userList, user];
+        this._updateUserList(userList);
+    }
+
+    removeUser(user: User) {
+        const userList = this._userList
+            .filter(_user => _user !== user);
+        this._updateUserList(userList);
+    }
+
+    undo() {
+        this._userList = this._userListHistory.pop();
+    }
+
+    private _updateUserList(userList: User[]) {
+        this._userListHistory.push(this._userList);
+        this._userList = userList;
+    }
+}
