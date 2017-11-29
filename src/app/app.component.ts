1fe2558971691f4d4c2472644188c2a67dc3674d
Younes JAAIDI <yjaaidi@gmail.com>
2017-11-29 15:10:29 +0100

    Playing with the timer + exercise boilerplate.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { User } from './user/user';
+
 
 @Component({
-  selector: 'wt-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'wt-app',
+    styleUrls: [
+        './app.component.css'
+    ],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'wt';
+
+    name = 'Foo';
+
+    userList = [
+        new User('Foo', 'BAR'),
+        new User('John', 'DOE')
+    ];
+
+    private _interval;
+
+    start() {
+
+        if (this._interval != null) {
+            return;
+        }
+
+        this._interval = setInterval(() => {
+            this.name += '.';
+        }, 500);
+
+    }
+
+    stop() {
+
+        if (this._interval != null) {
+            clearInterval(this._interval);
+            this._interval = null;
+        }
+
+    }
+
+    isStarted() {
+        return this._interval != null;
+    }
 }
