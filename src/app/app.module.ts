726b662d3870afc7d7f355f154a37391e92afb70
Younes JAAIDI <yjaaidi@gmail.com>
2017-11-30 10:53:42 +0100

    UserListComponent boilerplate.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { UserSelectorComponent } from './user/user-selector/user-selector.component';
 import { UserPreviewComponent } from './user/user-preview/user-preview.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 @NgModule({
     bootstrap: [
@@ -10,11 +13,13 @@ import { UserPreviewComponent } from './user/user-preview/user-preview.component
     ],
     declarations: [
         AppComponent,
-        UserSelectorComponent,
-        UserPreviewComponent
+        UserListComponent,
+        UserPreviewComponent,
+        UserSelectorComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ]
 })
 export class AppModule {
