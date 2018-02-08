6447d9c49aeb039719a282cbdf2331b800d1734c
Younes JAAIDI <yjaaidi@gmail.com>
2018-02-08 10:04:36 +0100

    Form validation.

--- src/app/user/user-form-reactive/user-form-reactive.component.ts
+++ src/app/user/user-form-reactive/user-form-reactive.component.ts
@@ -1,6 +1,22 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../user';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+
+const userBusinessIdValidator: ValidatorFn = (control: AbstractControl) => {
+
+    if (control.value == null) {
+        return null;
+    }
+
+    if (control.value.startsWith('wt')) {
+        return null;
+    }
+
+    return {
+        businessId: true
+    };
+
+};
 
 @Component({
     selector: 'wt-user-form-reactive',
@@ -14,9 +30,13 @@ export class UserFormReactiveComponent implements OnInit {
 
     formGroup = new FormGroup({
         firstName: new FormControl(null, [
-            Validators.required
+            Validators.required,
+            Validators.minLength(3),
         ]),
-        lastName: new FormControl(null),
+        lastName: new FormControl(null)
+        // userBusinessId: new FormControl(null, [
+        //     userBusinessIdValidator
+        // ])
         // email: new FormControl(null, [
         //     Validators.required,
         //     Validators.minLength(3)
@@ -30,7 +50,7 @@ export class UserFormReactiveComponent implements OnInit {
 
     }
 
-    getControls() {
+    getControlList() {
         return Object.values(this.formGroup.controls);
     }
 
