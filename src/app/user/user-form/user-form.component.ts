d2403a66dc3de4ede77dd99353668a33a7e36fae
Younes JAAIDI <yjaaidi@gmail.com>
2018-02-08 09:08:44 +0100

    `UserFormComponent`'s button label customization.

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../user';
 
 @Component({
@@ -8,6 +8,7 @@ import { User } from '../user';
 })
 export class UserFormComponent implements OnInit {
 
+    @Input() buttonLabel = 'SUBMIT';
     @Output() onUserSubmit = new EventEmitter<User>();
 
     user = new User();
