e07c2f2df39f8f74182a2d5623d3544ccc2adfc4
Younes JAAIDI <yjaaidi@gmail.com>
2018-02-27 17:01:01 +0100

    Angular bootstrap.

---
+++ user/user.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ *
+ * $Id: $
+ */
+exports.__esModule = true;
+var User = /** @class */ (function () {
+    function User(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    return User;
+}());
+exports.User = User;
