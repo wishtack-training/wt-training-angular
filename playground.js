cad99f6c1675a263e76151f6b835ede14170c856
Younes JAAIDI <yjaaidi@gmail.com>
2018-02-27 14:46:01 +0100

    Added TypeScript playground.

--- playground.js
+++ playground.js
@@ -4,39 +4,29 @@
  *
  * $Id: $
  */
-
 'use strict';
-
-class Customer {
-
-    constructor() {
-        this._firstName = 'Foo';
+var Customer = /** @class */ (function () {
+    function Customer() {
+        this.firstName = 'Foo';
     }
-
-    sayHi() {
-        console.log(this._firstName);
-    }
-
-    sayHiLater() {
-
-        setTimeout(() => {
-            this.sayHi();
+    Customer.prototype.sayHi = function () {
+        console.log(this.firstName);
+    };
+    Customer.prototype.sayHiLater = function () {
+        var _this = this;
+        setTimeout(function () {
+            _this.sayHi();
         }, 1000);
-
-    }
-
-}
-
-const debug = (data) => {
+    };
+    return Customer;
+}());
+var debug = function (data) {
     console.log(data);
 };
-
-const customer = new Customer();
-
+var customer = new Customer();
 customer.sayHi();
 customer.sayHiLater();
-
-const productList = [
+var productList = [
     {
         name: 'IntelliJ',
         price: 200
@@ -50,20 +40,40 @@ const productList = [
         price: 10
     }
 ];
-
-const byPrice = ({min = 10, max = 100}) => product => product.price > min && product.price < max;
-
-
-const cheapProductList = productList
-    .filter(byPrice({max: 37}));
-
-const cheapProductNameList = cheapProductList
-    .map(product => product.name);
-
-const totalPrice = cheapProductList
-    .map(product => product.price)
-    .reduce((sum, price) => sum + price, 0);
-
+var byPrice = function (_a) {
+    var _b = _a.min, min = _b === void 0 ? 10 : _b, _c = _a.max, max = _c === void 0 ? 100 : _c;
+    return function (product) { return product.price > min && product.price < max; };
+};
+var cheapProductList = productList
+    .filter(byPrice({ max: 37 }));
+var cheapProductNameList = cheapProductList
+    .map(function (product) { return product.name; });
+var totalPrice = cheapProductList
+    .map(function (product) { return product.price; })
+    .reduce(function (sum, price) { return sum + price; }, 0);
 console.log(cheapProductList);
 console.log(cheapProductNameList);
 console.log(totalPrice);
+var ShopAdmin = /** @class */ (function () {
+    function ShopAdmin() {
+    }
+    return ShopAdmin;
+}());
+var sendMessage = function (messagable) {
+    console.log('Hello ' + messagable.email);
+};
+sendMessage(new Customer());
+sendMessage(new ShopAdmin());
+sendMessage({
+    email: 'foo@wishtack.com'
+});
+var en = {
+    'HELLO': 'Hello',
+    'BYE': 'Bye'
+};
+var fr = {
+    'HELLO': 'Bonjour',
+    'BYE': 'Au revoir'
+};
+var foodType;
+foodType = 'burger';
---
+++ playground.ts
@@ -0,0 +1,112 @@
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ *
+ * $Id: $
+ */
+
+'use strict';
+
+class Customer {
+
+    firstName: string;
+    email: string;
+    address: string;
+
+    constructor() {
+        this.firstName = 'Foo';
+    }
+
+    sayHi() {
+        console.log(this.firstName);
+    }
+
+    sayHiLater() {
+
+        setTimeout(() => {
+            this.sayHi();
+        }, 1000);
+
+    }
+
+}
+
+const debug = (data) => {
+    console.log(data);
+};
+
+const customer = new Customer();
+
+customer.sayHi();
+customer.sayHiLater();
+
+const productList = [
+    {
+        name: 'IntelliJ',
+        price: 200
+    },
+    {
+        name: 'BrowserStack',
+        price: 30
+    },
+    {
+        name: 'Keyboard',
+        price: 10
+    }
+];
+
+const byPrice = ({min = 10, max = 100}) => product => product.price > min && product.price < max;
+
+
+const cheapProductList = productList
+    .filter(byPrice({max: 37}));
+
+const cheapProductNameList = cheapProductList
+    .map(product => product.name);
+
+const totalPrice = cheapProductList
+    .map(product => product.price)
+    .reduce((sum, price) => sum + price, 0);
+
+console.log(cheapProductList);
+console.log(cheapProductNameList);
+console.log(totalPrice);
+
+
+class ShopAdmin {
+    name: string;
+    email: string;
+    address: string;
+}
+
+interface Messagable {
+    email: string;
+    address?: string;
+}
+
+const sendMessage = (messagable: Messagable) => {
+    console.log('Hello ' + messagable.email);
+};
+
+sendMessage(new Customer());
+sendMessage(new ShopAdmin());
+
+sendMessage({
+    email: 'foo@wishtack.com'
+});
+
+const en = {
+    'HELLO': 'Hello',
+    'BYE': 'Bye'
+};
+
+type TranslationData = typeof en;
+
+const fr: TranslationData = {
+    'HELLO': 'Bonjour',
+    'BYE': 'Au revoir'
+};
+
+let foodType: '🍔' | '🥦' | 'burger';
+
+foodType = 'burger';
