43fa027ac409414dd078dd9be75120fa427030e6
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-07 11:31:46 +0100

    Added `DemoComponent`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'wt-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'wt-app',
+    templateUrl: './app.component.html',
+    styleUrls: [
+        './app.component.css'
+    ]
 })
 export class AppComponent {
-  title = 'wt';
+    
 }
+
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wt-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
