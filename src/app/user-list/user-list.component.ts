e05d26732607644f4ef9357f944d38fe021d59c7
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-14 12:14:09 +0100

    Added `UserListComponent` skeleton.

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserStore } from '../user/user-store';
+import { User } from '../user/user';
 
 @Component({
-  selector: 'wt-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+    selector: 'wt-user-list',
+    templateUrl: './user-list.component.html',
+    styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent {
 
-  constructor() { }
+    private _userStore = new UserStore();
 
-  ngOnInit() {
-  }
+    constructor() {
+        this._userStore.addUser(new User('Foo', 'BAR'));
+        this._userStore.addUser(new User('John', 'DOE'));
+    }
 
 }
