889915a73a8943224ebc16aeca3d2be729784d9a
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-14 14:24:26 +0100

    Added `UserListComponent` using ngModel 🤢.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DemoComponent,
-    UserListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DemoComponent,
+        UserListComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -9,11 +9,30 @@ import { User } from '../user/user';
 })
 export class UserListComponent {
 
+    userTmp = new User();
+
     private _userStore = new UserStore();
 
     constructor() {
         this._userStore.addUser(new User('Foo', 'BAR'));
         this._userStore.addUser(new User('John', 'DOE'));
+    }
+
+    addUser() {
+        this._userStore.addUser(this.userTmp);
+        this.userTmp = new User();
+    }
+
+    getPictureUrl(user: User) {
+        return `https://robohash.org/${user.firstName}?set=set4`;
+    }
+
+    getUserList() {
+        return this._userStore.getUserList();
+    }
+
+    removeUser(user: User) {
+        this._userStore.removeUser(user);
     }
 
 }
