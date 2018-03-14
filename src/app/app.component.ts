f89834dd239eeab15448516f7c31ebe942bd2125
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-14 11:56:24 +0100

    Added `DemoComponent`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'wt-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'wt-app',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'wt';
 }
