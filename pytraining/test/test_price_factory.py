eeeb7a984c8d4d6a2614b6f6b362baadf94d166d
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-21 14:55:29 +0100

    Added empty python `PriceFactory` spec.

---
+++ pytraining/test/test_price_factory.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# (c) 2013-2018 Wishtack
+#
+# $Id: $
+#
+
+from unittest import TestCase, skip
+
+
+class TestPriceFactory(TestCase):
+
+    @skip(u"Not implemented yet")
+    def test_create_price_with_amount(self):
+        pass
+
