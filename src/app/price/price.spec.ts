d2a7588a028665f0eff7f01aff78dfc77116e97c
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-21 12:09:01 +0100

    Added empty Price specs blueprint.

--- src/app/price/price.spec.ts
+++ src/app/price/price.spec.ts
@@ -8,16 +8,25 @@
 describe('Price', () => {
 
     xit('should create price from coefficient and exponent', () => {
-
+        // @TODO: new Price(coefficient=1050, exponent=-2, currency='USD');
+        // @TODO: check price.amount 10.5
+        // @TODO: check price.currency
+        // @TODO: check price.coefficient
+        // @TODO: check price.exponent
     });
 
     xit('should create price from amount', () => {
+        // @TODO: new Price(amount=10.505, currency='USD');
+        // @TODO: check price.amount 10.5
+        // @TODO: check price.currency
+        // @TODO: check price.coefficient 1050
+        // @TODO: check price.exponent -2
 
     });
 
     xit('should create price from raw string', () => {
-
+        // @TODO: new Price(raw='10.5 $')
+        // ...
     });
 
 });
-
