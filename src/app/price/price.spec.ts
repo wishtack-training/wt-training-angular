b3909ac878a0797493cbf62b1ab91c74b4612674
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-21 12:04:37 +0100

    Added empty Price specs.

---
+++ src/app/price/price.spec.ts
@@ -0,0 +1,23 @@
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ *
+ * $Id: $
+ */
+
+describe('Price', () => {
+
+    xit('should create price from coefficient and exponent', () => {
+
+    });
+
+    xit('should create price from amount', () => {
+
+    });
+
+    xit('should create price from raw string', () => {
+
+    });
+
+});
+
