17323211b71690e95b55b8000ebd27cfd388401a
Younes JAAIDI <yjaaidi@gmail.com>
2018-03-22 11:45:55 +0100

    Added `StatsHelper` empty specs.

---
+++ src/app/stats/stats-helper.spec.ts
@@ -0,0 +1,18 @@
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ *
+ * $Id: $
+ */
+
+describe('StatsHelper', () => {
+
+    xit('should calculate average', () => {
+
+    });
+
+    xit('should raise exception if data array is empty', () => {
+
+    });
+
+});
---
+++ src/app/stats/stats.module.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ *
+ * $Id: $
+ */
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+    declarations: [],
+    exports: [],
+    imports: []
+})
+export class StatsModule {
+}
