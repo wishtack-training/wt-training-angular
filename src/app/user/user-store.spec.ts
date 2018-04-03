072acc7c0e11423d2bf7c85b7003f9d0ed0791ca
Younes JAAIDI <yjaaidi@gmail.com>
2018-04-03 15:38:31 +0200

    Added UserStore specs.

--- src/app/user/user-store.spec.ts
+++ src/app/user/user-store.spec.ts
@@ -5,13 +5,54 @@
  * $Id: $
  */
 
+import { User } from './user';
+import { UserStore } from './user-store';
+
+
 describe('UserStore', () => {
 
     xit('should add users', () => {
 
+        const userStore = new UserStore();
+
+        const user1 = new User('Foo', 'BAR');
+        const user2 = new User('John', 'DOE');
+        const user3 = new User('Foo', 'BAR');
+
+        // @TODO
+        // expect(userStore.getUserList()).toEqual([]);
+        //
+        // userStore.addUser(user1);
+        // userStore.addUser(user2);
+        // userStore.addUser(user3);
+        //
+        // expect(userStore.getUserList()).toEqual([
+        //     user1,
+        //     user2,
+        //     user3
+        // ]);
+
     });
 
     xit('should remove users', () => {
+
+        const userStore = new UserStore();
+
+        const user1 = new User('Foo', 'BAR');
+        const user2 = new User('John', 'DOE');
+        const user3 = new User('Foo', 'BAR');
+
+        // @TODO
+        // userStore.addUser(user1);
+        // userStore.addUser(user2);
+        // userStore.addUser(user3);
+        //
+        // userStore.removeUser(user1);
+        //
+        // expect(userStore.getUserList()).toEqual([
+        //     user2,
+        //     user3
+        // ]);
 
     });
 
---
+++ src/app/user/user.ts
@@ -0,0 +1,7 @@
+export class User {
+
+    constructor(firstName: string, lastName: string) {
+        throw new Error('Not implemented yet!');
+    }
+
+}
