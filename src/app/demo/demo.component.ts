adf4a38b9c8c23ab233ca1d97b1671785244ab16
Younes JAAIDI <yjaaidi@gmail.com>
2018-04-04 10:21:48 +0200

    Added demo component.

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'wt-demo',
     templateUrl: './demo.component.html',
     styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent {
+
+    userName = 'Foo';
+
+    cityList = [
+        'Dijon',
+        'Grenoble',
+        'Lyon'
+    ];
 
     constructor() {
+        // @HACK: Never do this... specially in the constructor...
+        setInterval(() => {
+            this.userName += '.';
+        }, 1000);
+
     }
 
-    ngOnInit() {
+    canReset() {
+        return this.userName.length < 5;
+    }
+
+    reset() {
+        this.userName = '';
     }
 
 }
