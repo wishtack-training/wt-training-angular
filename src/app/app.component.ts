f0856e2482b4e0fd9a70b46515fee7e9abc735cd
Younes JAAIDI <yjaaidi@gmail.com>
2018-04-04 09:55:50 +0200

    Added empty demo component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'wt-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'wt-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wt';
 }
