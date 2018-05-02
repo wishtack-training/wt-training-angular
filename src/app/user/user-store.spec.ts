d5fb9241c53535388e435d4609f08b5b23b00cb9
Younes JAAIDI <yjaaidi@gmail.com>
2018-05-02 16:40:21 +0200

    Added broken tests.

--- src/app/user/user-store.spec.ts
+++ src/app/user/user-store.spec.ts
@@ -7,11 +7,52 @@
 
 describe('UserStore', () => {
 
-    xit('should add users', () => {
+    it('should add users', () => {
+
+        const userStore = new UserStore();
+
+        const user1 = new User('Foo', 'BAR');
+        const user2 = new User('John', 'DOE');
+        const user3 = new User('Foo', 'BAR');
+
+        const userListEmpty = userStore.getUserList();
+
+        expect(userListEmpty).toEqual([]);
+
+        userStore.addUser(user1);
+        userStore.addUser(user2);
+        userStore.addUser(user3);
+
+        const userList = userStore.getUserList();
+
+        expect(userList).toEqual([
+            user1,
+            user2,
+            user3
+        ]);
 
     });
 
-    xit('should remove users', () => {
+    it('should remove users', () => {
+
+        const userStore = new UserStore();
+
+        const user1 = new User('Foo', 'BAR');
+        const user2 = new User('John', 'DOE');
+        const user3 = new User('Foo', 'BAR');
+
+        userStore.addUser(user1);
+        userStore.addUser(user2);
+        userStore.addUser(user3);
+
+        userStore.removeUser(user1);
+
+        const userList = userStore.getUserList();
+
+        expect(userList).toEqual([
+            user2,
+            user3
+        ]);
 
     });
 
