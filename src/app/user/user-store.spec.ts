7e358225e402a0365ad763715366cf19c1092bbb
Younes JAAIDI <yjaaidi@gmail.com>
2018-05-02 16:36:07 +0200

    Added empty tests.

---
+++ src/app/user/user-store.spec.ts
@@ -0,0 +1,18 @@
+/**
+ *
+ * (c) 2013-2018 Wishtack
+ *
+ * $Id: $
+ */
+
+describe('UserStore', () => {
+
+    xit('should add users', () => {
+
+    });
+
+    xit('should remove users', () => {
+
+    });
+
+});
