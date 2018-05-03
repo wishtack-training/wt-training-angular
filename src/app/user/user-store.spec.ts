bad8098816a9f33f245c7680dabd929466418d05
Younes JAAIDI <yjaaidi@gmail.com>
2018-05-03 10:16:54 +0200

    UserStore immutable implementation.

--- src/app/user/user-store.spec.ts
+++ src/app/user/user-store.spec.ts
@@ -3,23 +3,32 @@ import { UserStore } from './user-store';
 
 describe('UserStore', () => {
 
+    let userStore: UserStore;
+    let user1: User;
+    let user2: User;
+    let user3: User;
+
+    beforeEach(() => {
+
+        userStore = new UserStore();
+
+        user1 = new User('Foo', 'BAR');
+        user2 = new User('John', 'DOE');
+        user3 = new User('Foo', 'BAR');
+
+    });
+
     it('should add users', () => {
 
-        const userStore = new UserStore();
-
-        const user1 = new User('Foo', 'BAR');
-        const user2 = new User('John', 'DOE');
-        const user3 = new User('Foo', 'BAR');
-
         const userListEmpty = userStore.getUserList();
-
-        expect(userListEmpty).toEqual([]);
 
         userStore.addUser(user1);
         userStore.addUser(user2);
         userStore.addUser(user3);
 
         const userList = userStore.getUserList();
+
+        expect(userListEmpty).toEqual([]);
 
         expect(userList).toEqual([
             user1,
@@ -31,19 +40,21 @@ describe('UserStore', () => {
 
     it('should remove users', () => {
 
-        const userStore = new UserStore();
-
-        const user1 = new User('Foo', 'BAR');
-        const user2 = new User('John', 'DOE');
-        const user3 = new User('Foo', 'BAR');
-
         userStore.addUser(user1);
         userStore.addUser(user2);
         userStore.addUser(user3);
 
+        const userListFull = userStore.getUserList();
+
         userStore.removeUser(user1);
 
         const userList = userStore.getUserList();
+
+        expect(userListFull).toEqual([
+            user1,
+            user2,
+            user3
+        ]);
 
         expect(userList).toEqual([
             user2,
