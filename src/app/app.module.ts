e572846ab8899452838672fcdf13c4ac63b49a87
Younes JAAIDI <yjaaidi@gmail.com>
2018-06-07 11:56:17 +0200

    Added book form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { BookPreviewComponent } from './book/book-preview/book-preview.component
         BookPreviewComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        ReactiveFormsModule
     ],
     bootstrap: [AppComponent]
 })
--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Book } from '../book';
 import { BookRepository } from '../book-repository';
 
@@ -9,6 +10,13 @@ import { BookRepository } from '../book-repository';
 })
 export class BookListComponent implements OnInit {
 
+    bookForm = new FormGroup({
+        title: new FormControl(null, [
+            Validators.required,
+            Validators.minLength(3)
+        ]),
+        pictureUrl: new FormControl()
+    });
     bookList: Book[];
 
     constructor(private _bookRepository: BookRepository) {
@@ -18,4 +26,24 @@ export class BookListComponent implements OnInit {
         this.bookList = this._bookRepository.getBookList();
     }
 
+    addBook() {
+        const book = new Book(this.bookForm.value);
+        this.bookList = [...this.bookList, book];
+        this.bookForm.reset();
+    }
+
+    getErrorMessageList() {
+
+        return Object.entries(this.bookForm.controls)
+            .filter(([name, control]) => {
+                return control.touched && control.errors != null;
+            })
+            .map(([name, control]) => {
+                if (control.hasError('required')) {
+                    return `${name} is required.`;
+                }
+                return `${name} is invalid.`;
+            });
+
+    }
 }
