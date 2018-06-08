588b75b995b492546a454fc735c0b3c41c50995c
Younes JAAIDI <yjaaidi@gmail.com>
2018-06-08 11:50:31 +0200

    Angular Material setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { BookSearchComponent } from './book/book-list/book-search.component';
 import { BookPreviewComponent } from './book/book-preview/book-preview.component';
 import { BookFormComponent } from './book/book-form/book-form.component';
 import { AuthorFormComponent } from './author/author-form/author-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -19,7 +21,12 @@ import { AuthorFormComponent } from './author/author-form/author-form.component'
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         HttpClientModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
         ReactiveFormsModule
     ],
     bootstrap: [AppComponent]
