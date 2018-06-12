b15c115bd4a046ef052b675e96c34a7aea8394d7
Younes JAAIDI <yjaaidi@gmail.com>
2018-06-13 01:51:00 +0200

    Added book-store.spec.ts.

---
+++ src/app/book/book-store.spec.ts
@@ -0,0 +1,52 @@
+describe('BookStore', () => {
+
+    it('should add books', () => {
+
+        const bookStore = new BookStore();
+
+        const bookListEmpty = bookStore.getBookList();
+
+        const book1 = new Book('eXtreme Programming Explained');
+        const book2 = new Book('ReWork');
+        const book3 = new Book('Clean Code');
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookListEmpty).toEqual([]);
+
+        expect(bookList).toEqual([
+            book1,
+            book2,
+            book3
+        ]);
+
+    });
+
+    it('should remove books', () => {
+
+        const bookStore = new BookStore();
+
+        const book1 = new Book('eXtreme Programming Explained');
+        const book2 = new Book('ReWork');
+        const book3 = new Book('Clean Code');
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        bookStore.removeBook(book2);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookList).toEqual([
+            book1,
+            book3
+        ]);
+
+    });
+
+});
