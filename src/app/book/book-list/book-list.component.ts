3c2ceed48a12c9c8d3dfd939cb91fb686c6d7a42
Younes JAAIDI <yjaaidi@gmail.com>
2018-06-13 14:05:41 +0200

    Added <wt-book-list> skeleton.

---
+++ src/app/book/book-list/book-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BookStore } from '../book-store';
+
+@Component({
+    selector: 'wt-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+    private _bookStore = new BookStore();
+
+    constructor() {
+        this._bookStore.addBook(
+            new Book('A')
+        );
+    }
+
+    ngOnInit() {
+    }
+
+}
