a5f1b0c05185c408bce6add30bf547e46f226fa1
Younes JAAIDI <yjaaidi@gmail.com>
2018-06-13 14:00:13 +0200

    Added <wt-book-list> skeleton.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'wt-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'wt-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'wt';
+
+    messageList = [];
+    title = 'Loading';
+    themeList = [
+        {
+            backgroundColor: '#000000cc',
+            color: 'white'
+        },
+        {
+            backgroundColor: 'white',
+            color: 'black'
+        }
+    ];
+    selectedTheme;
+
+    constructor() {
+        setInterval(() => {
+            this.title += '.';
+
+            this.messageList = [...this.messageList, 'Hello!'];
+
+        }, 500);
+    }
+
+    canResetTitle() {
+        return this.title.length > 10;
+    }
+
+    resetEverything() {
+        this.title = 'Loading';
+        this.messageList = [];
+    }
+
+    selectTheme(theme) {
+        this.selectedTheme = theme;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule
