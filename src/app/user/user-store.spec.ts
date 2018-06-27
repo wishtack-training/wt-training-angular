ae3a3ef6faab0563f0f757037c9ea71bc6ce4091
Younes JAAIDI <yjaaidi@gmail.com>
2018-06-27 09:51:50 +0200

    Added UserStore spec.

---
+++ src/app/user/user-store.spec.ts
@@ -0,0 +1,65 @@
+
+describe('UserStore', () => {
+
+    let userStore: UserStore;
+
+    let user1: User;
+    let user2: User;
+    let user3: User;
+
+    beforeEach(() => {
+
+        userStore = new UserStore();
+
+        user1 = new User('Foo', 'BAR');
+        user2 = new User('John', 'DOE');
+        user3 = new User('Foo', 'BAR');
+
+    });
+
+    it('should add users', () => {
+
+        const userListEmpty = userStore.getUserList();
+
+        expect(userListEmpty).toEqual([]);
+
+        userStore.addUser(user1);
+        userStore.addUser(user2);
+        userStore.addUser(user3);
+
+        const userList = userStore.getUserList();
+
+        expect(userList).toEqual([
+            user1,
+            user2,
+            user3
+        ]);
+
+    });
+
+    it('should remove users', () => {
+
+        userStore.addUser(user1);
+        userStore.addUser(user2);
+        userStore.addUser(user3);
+
+        const userList = userStore.getUserList();
+
+        expect(userList).toEqual([
+            user1,
+            user2,
+            user3
+        ]);
+
+        userStore.removeUser(user1);
+
+        const userListPartial = userStore.getUserList();
+
+        expect(userListPartial).toEqual([
+            user2,
+            user3
+        ]);
+
+    });
+
+});
