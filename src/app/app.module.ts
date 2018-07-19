07ee11610bfa99da39f1c64a58de9f056de28458
Younes JAAIDI <yjaaidi@gmail.com>
2018-07-19 13:51:36 +0200

    Angular material setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         UserModule
     ],
     bootstrap: [AppComponent]
