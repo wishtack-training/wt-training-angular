064f25c9c69c19e497f2eec90265dbd14d136fd3
Younes JAAIDI <yjaaidi@gmail.com>
2018-09-19 16:23:41 +0200

    Added empty specs.

---
+++ src/app/user/user-store.spec.ts
@@ -0,0 +1,13 @@
+
+describe('UserStore', () => {
+
+    xit('should add users', () => {
+        throw new Error('😱 Not implemented yet!');
+    });
+
+    xit('should remove users', () => {
+        throw new Error('😱 Not implemented yet!');
+    });
+
+});
+
