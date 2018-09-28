0dd71d4a42a4de0b659ef77e144efed60c546d05
Younes JAAIDI <yjaaidi@gmail.com>
2018-09-28 18:52:45 +0200

    Meanwhile lib-a is deployed for the first time, using local build: `yarn workspace demo add file:dist/lib-a`.

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LibAModule } from 'lib-a';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        LibAModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
