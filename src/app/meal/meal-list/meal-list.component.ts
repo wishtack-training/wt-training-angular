9603bdb50adde35de0c906f39971ef07e17d122b
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-03 17:59:26 +0200

    Form validation.

--- src/app/meal/meal-list/meal-list.component.ts
+++ src/app/meal/meal-list/meal-list.component.ts
@@ -1,26 +1,81 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Meal } from '../meal';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Meal, MealType } from '../meal';
 import { MealStore } from '../meal-store';
+
+export function createNameControl() {
+    return new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3)
+    ]);
+}
+
+export const rangeValidator = (minValue: number, maxValue: number) => (control: AbstractControl) => {
+
+    const value = control.value;
+
+    if (value == null) {
+        return null;
+    }
+
+    if (value < minValue) {
+        return {
+            rangeInvalid: {
+                expectedMinValue: minValue,
+                value
+            }
+        };
+    }
+
+    if (value > maxValue) {
+        return {
+            rangeInvalid: {
+                expectedMaxValue: maxValue,
+                value
+            }
+        };
+    }
+
+    return null;
+
+};
 
 @Component({
     selector: 'wt-meal-list',
     templateUrl: './meal-list.component.html',
     styleUrls: ['./meal-list.component.css']
 })
-export class MealListComponent {
+export class MealListComponent implements OnInit {
 
     mealForm = new FormGroup({
-        name: new FormControl(),
-        price: new FormControl(),
-        type: new FormControl()
-    });
+        name: createNameControl(),
+        price: new FormControl(null, [
+            rangeValidator(5, 10)
+        ]),
+        type: new FormControl(null)
+    }, [
+        (control) => {
+
+            if (control.value.type === MealType.desert && control.value.price > 6) {
+                return {
+                    wtf: true
+                };
+            }
+
+            return null;
+
+        }
+    ]);
 
     private _mealStore = new MealStore();
+
+    ngOnInit() {
+    }
 
     addMeal() {
         const meal = new Meal(this.mealForm.value);
         this._mealStore.addMeal(meal);
+        this.mealForm.reset();
     }
 
     getMealList() {
