0d4498c12801c1148fe32857bdb46af2a9ff5c72
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-09 16:12:26 +0200

    Angular material setup (`yarn ng add @angular/material`).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
 import { BookListComponent } from './book/book-list/book-list.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { BookListComponent } from './book/book-list/book-list.component';
         BookListComponent
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         ReactiveFormsModule
     ],
