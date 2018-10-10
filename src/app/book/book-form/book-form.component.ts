d5d9153a68c3199644d2730d07a67bc3c529c8a8
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-10 15:43:26 +0200

    Inline book edition.

--- src/app/book/book-form/book-form.component.ts
+++ src/app/book/book-form/book-form.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { rangeValidator } from '../../../lib/range.validator';
-import { Author } from '../../author/author';
 import { Book } from '../book';
 
 @Component({
