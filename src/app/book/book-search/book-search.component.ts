d7f5f4dcab59930701c6215be87964a3852a18b8
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-12 10:27:07 +0200

    Observables refactoring + added RxScavenger.

--- src/app/book/book-search/book-search.component.ts
+++ src/app/book/book-search/book-search.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, switchMap } from 'rxjs/operators';
+import { Scavenger } from '@wishtack/rx-scavenger';
+import { interval, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { Book } from '../book';
 import { GoogleBookRepository } from '../google-book-repository.service';
 
@@ -9,29 +11,48 @@ import { GoogleBookRepository } from '../google-book-repository.service';
     templateUrl: './book-search.component.html',
     styleUrls: ['./book-search.component.css']
 })
-export class BookSearchComponent implements OnInit {
+export class BookSearchComponent implements OnDestroy, OnInit {
 
     bookList: Book[];
     keywordsControl = new FormControl();
+    time$ = interval(1000)
+        .pipe(map(() => new Date()));
+
+    private _bookList$: Observable<Book[]>;
+    private _keywords$: Observable<string>;
+    private _scavenger = new Scavenger(this);
 
     constructor(
         private _googleBookRepository: GoogleBookRepository
     ) {
+
+        this._keywords$ = this.keywordsControl.valueChanges
+            .pipe(
+                debounceTime(200),
+                distinctUntilChanged()
+            );
+
+        this._bookList$ = this._keywords$
+            .pipe(
+                switchMap(keywords => {
+                    return this._googleBookRepository.searchBooks(keywords);
+                })
+            );
     }
 
     ngOnInit() {
 
-        this.keywordsControl.valueChanges
+        this._bookList$
             .pipe(
-                debounceTime(200),
-                switchMap((keywords) => {
-                    return this._googleBookRepository.searchBooks(keywords);
-                })
+                this._scavenger.collect()
             )
-            .subscribe(bookList => {
+            .subscribe((bookList) => {
                 this.bookList = bookList;
             });
 
+    }
+
+    ngOnDestroy() {
     }
 
 }
