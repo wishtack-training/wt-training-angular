cd5bb5db4a6f820d6543785b83fb4f3c98de64ae
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-16 15:09:44 +0200

    Webpack html setup.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "src/index.html"
