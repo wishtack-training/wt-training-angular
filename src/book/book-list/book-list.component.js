e52db26d4784b5b90cd8053fbe6914eef8931078
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-16 15:35:26 +0200

    Added `<wt-book-list>`.

--- src/book/book-list/book-list.component.js
+++ src/book/book-list/book-list.component.js
@@ -8,11 +8,34 @@
 'use strict';
 
 import {BookStore} from '../book-store';
+import {Book} from '../book';
 
 export class BookListComponent {
 
     constructor() {
+        this.bookForm = null;
+        this.tmpBook = new Book();
         this._bookStore = new BookStore();
+    }
+
+    addBook() {
+
+        this._bookStore.addBook(this.tmpBook);
+
+        /* Rest the form. */
+        this.tmpBook = new Book();
+
+        /* Reset form's pristine state as if we never touched it. */
+        this.bookForm.$setPristine();
+
+    }
+
+    getBookList() {
+        return this._bookStore.getBookList();
+    }
+
+    removeBook(book) {
+        this._bookStore.removeBook(book);
     }
 
 }
