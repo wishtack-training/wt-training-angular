a3a0e0a65ce3d4c6c60cbcc5092654464d421ea3
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-17 15:25:04 +0200

    Router setup.

--- src/app.module.js
+++ src/app.module.js
@@ -1,14 +1,29 @@
 import angular from 'angular';
+import ngRouteName from 'angular-route';
 import {HelloComponent} from './hello/hello.component';
 import {bookModule} from './book/book.module';
 
 
 export const appModule = angular.module('app', [
-    bookModule.name
+    bookModule.name,
+    ngRouteName
 ]);
 
 appModule.component('wtHello', HelloComponent.config);
 
 appModule.component('wtSeparator', {
     template: '<hr>'
+});
+
+appModule.config(($routeProvider) => {
+
+    $routeProvider
+        .when('/hello', {
+            template: '<wt-hello></wt-hello>'
+        })
+        .when('/not-found', {
+            template: '<h1>😞</h1>'
+        })
+        .otherwise('/not-found');
+
 });
