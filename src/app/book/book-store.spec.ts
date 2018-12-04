d8ef8e6448f8cdd6c73d8a494b3196f1f00feffb
Younes JAAIDI <yjaaidi@gmail.com>
2018-12-04 16:17:17 +0100

    Added `BookStore.addBook()`.

--- src/app/book/book-store.spec.ts
+++ src/app/book/book-store.spec.ts
@@ -1,11 +1,83 @@
+class BookStore {
+
+    private _bookList: Book[] = [];
+
+    getBookList() {
+        return this._bookList;
+    }
+
+    addBook(book: Book) {
+        this._bookList.push(book);
+    }
+
+    /**
+     * @deprecated Work in progress
+     */
+    removeBook(book: Book) {
+        throw new Error('😱 Not implemented yet!');
+    }
+}
+
+class Book {
+
+    constructor(title: string, authorName: string) {
+
+    }
+
+}
+
 describe('BookStore', () => {
 
-    xit('should add books', () => {
-        throw new Error('😱 Not implemented yet!');
+    let bookStore: BookStore;
+
+    let book1: Book;
+    let book2: Book;
+    let book3: Book;
+
+    beforeEach(() => {
+
+        bookStore = new BookStore();
+
+        book1 = new Book('eXtreme Programming', 'Kent Beck');
+        book2 = new Book('ReWork', 'Jason Fried');
+        book3 = new Book('eXtreme Programming', 'Kent Beck');
+
+    });
+
+    it('should add books', () => {
+
+        const bookListEmpty = bookStore.getBookList();
+
+        expect(bookListEmpty).toEqual([]);
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookList).toEqual([
+            book1,
+            book2,
+            book3
+        ]);
+
     });
 
     xit('should remove books', () => {
-        throw new Error('😱 Not implemented yet!');
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        bookStore.removeBook(book1);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookList).toEqual([
+            book2,
+            book3
+        ]);
     });
 
 });
