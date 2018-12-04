cc56fffb94fe08c55f5b27f6ae1d4d39196e8cbb
Younes JAAIDI <yjaaidi@gmail.com>
2018-12-04 15:20:11 +0100

    Added `BookStore` empty specs.

---
+++ src/app/book/book-store.spec.ts
@@ -0,0 +1,11 @@
+describe('BookStore', () => {
+
+    xit('should add books', () => {
+        throw new Error('😱 Not implemented yet!');
+    });
+
+    xit('should remove books', () => {
+        throw new Error('😱 Not implemented yet!');
+    });
+
+});
