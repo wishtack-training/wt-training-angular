414d75988c25b870cd85965323ec9351b9264260
Younes JAAIDI <yjaaidi@gmail.com>
2018-12-10 15:57:47 +0100

    Added `BookStore` specs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'wt-root',
     templateUrl: './app.component.html',
--- src/app/book/book-store.spec.ts
+++ src/app/book/book-store.spec.ts
@@ -1,11 +1,57 @@
+
 describe('BookStore', () => {
 
-    xit('should add books', () => {
-        throw new Error('😱 Not implemented yet!');
+    let bookStore: BookStore;
+
+    let book1: Book;
+    let book2: Book;
+    let book3: Book;
+
+    beforeEach(() => {
+
+        bookStore = new BookStore();
+
+        book1 = new Book('eXtreme Programming Explained', 'Kent Beck');
+        book2 = new Book('ReWork', 'Jason Fried');
+        book3 = new Book('eXtreme Programming Explained', 'Kent Beck');
+
     });
 
-    xit('should remove books', () => {
-        throw new Error('😱 Not implemented yet!');
+    it('should add books', () => {
+
+        const bookListEmpty = bookStore.getBookList();
+
+        expect(bookListEmpty).toEqual([]);
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookList).toEqual([
+            book1,
+            book2,
+            book3
+        ]);
+
+    });
+
+    it('should remove books', () => {
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        bookStore.removeBook(book1);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookList).toEqual([
+            book2,
+            book3
+        ]);
+
     });
 
 });
