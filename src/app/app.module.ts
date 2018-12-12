08cca637bd6eaa774593255a17baff6d78679e68
Younes JAAIDI <yjaaidi@gmail.com>
2018-12-12 16:07:01 +0100

    Promise vs. async/await vs. Observable.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,6 +16,7 @@ import { PlaygroundModule } from './playground/playground.module';
         BookModule,
         BrowserAnimationsModule,
         BrowserModule,
+        HttpClientModule,
         PlaygroundModule,
         ReactiveFormsModule
     ],
--- src/app/playground/demo/demo.component.ts
+++ src/app/playground/demo/demo.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { User } from '../form-demo/form-demo.component';
 
 @Component({
@@ -6,7 +8,7 @@ import { User } from '../form-demo/form-demo.component';
     templateUrl: './demo.component.html',
     styleUrls: ['./demo.component.scss']
 })
-export class DemoComponent {
+export class DemoComponent implements OnDestroy, OnInit {
 
     cityList = [
         'Dijon',
@@ -15,11 +17,80 @@ export class DemoComponent {
 
     title = 'Hello!';
 
-    constructor() {
-        /* @hack Don't do this at home.  🤮. */
-        setInterval(() => {
-            this.title += '.';
-        }, 200);
+    private _subscription: Subscription;
+
+    constructor(private _httpClient: HttpClient) {
+    }
+
+    ngOnInit() {
+
+        this._subscription = this._httpClient.get('https://www.googleapis.com/books/v1/volumes?q=eXtreme%20Programming')
+            .subscribe(data => {
+                console.log(data);
+            });
+
+
+        // const data$ = interval(100)
+        //     .pipe(
+        //         map(data => data % 10),
+        //         filter(data => data !== 0),
+        //         bufferTime(500),
+        //         tap(data => {
+        //             if (data[0] === 4) {
+        //                 throw new Error('test');
+        //             }
+        //         })
+        //     );
+        //
+        // const subscription = data$
+        //     .pipe(
+        //         retry(2)
+        //     )
+        //     .subscribe({
+        //         next: data => console.log(data),
+        //         error: err => console.log('Something went wrong'),
+        //         complete: () => {
+        //             console.log('DONE');
+        //         }
+        //     });
+        //
+        // setTimeout(() => subscription.unsubscribe(), 2000);
+
+        // try {
+        //
+        //     const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=eXtreme%20Programming');
+        //
+        //     const data = await response.json();
+        //
+        //     console.log(data);
+        //
+        // } catch (e) {
+        //     console.log('ERROR');
+        // }
+
+        // function wait(duration) {
+        //     return new Promise(resolve => {
+        //        setTimeout(resolve, duration);
+        //     });
+        // }
+        //
+        // await wait(1000);
+
+        // fetch('https://www.googleapis.com/books/v1/volumes?q=eXtreme%20Programming')
+        //     .then(response => {
+        //         return response.json();
+        //     })
+        //     .then(data => {
+        //         console.log(data);
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //     });
+
+    }
+
+    ngOnDestroy() {
+        this._subscription.unsubscribe();
     }
 
     reset() {
