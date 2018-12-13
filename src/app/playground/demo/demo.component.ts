5a6c77254942ab2721d9141cc018ce6ae7a2425c
Younes JAAIDI <yjaaidi@gmail.com>
2018-12-13 11:59:44 +0100

    Added more observable examples.

--- src/app/playground/demo/demo.component.ts
+++ src/app/playground/demo/demo.component.ts
@@ -1,7 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { map, pairwise } from 'rxjs/operators';
 import { User } from '../form-demo/form-demo.component';
+
+export interface BookApiListResponse<T> {
+    totalItems: number;
+    items?: T[];
+}
+
+export interface BookApiVolume {
+    id: string;
+    volumeInfo: {
+        title: string;
+        authors: Array<string>
+    };
+}
+
+export type BookApiVolumeListResponse = BookApiListResponse<BookApiVolume>;
+
 
 @Component({
     selector: 'wt-demo',
@@ -17,6 +35,8 @@ export class DemoComponent implements OnDestroy, OnInit {
 
     title = 'Hello!';
 
+    keywordsControl = new FormControl();
+
     private _subscription: Subscription;
 
     constructor(private _httpClient: HttpClient) {
@@ -24,9 +44,21 @@ export class DemoComponent implements OnDestroy, OnInit {
 
     ngOnInit() {
 
-        this._subscription = this._httpClient.get('https://www.googleapis.com/books/v1/volumes?q=eXtreme%20Programming')
+        this.keywordsControl.valueChanges
+            .pipe(
+                pairwise(),
+                map(([previous, current]) => {
+                    return current.length - previous.length;
+                })
+            )
+            .subscribe(data => console.log(data));
+
+        const url = 'https://www.googleapis.com/books/v1/volumes?q=eXtreme%20Programming';
+
+
+        this._subscription = this._httpClient.get<BookApiVolumeListResponse>(url)
             .subscribe(data => {
-                console.log(data);
+                console.log(data.totalItems);
             });
 
 
