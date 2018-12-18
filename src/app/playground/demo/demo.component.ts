ef2caa8a1603e9dc5bfe769c87ac87c2e6d883b4
Younes JAAIDI <yjaaidi@gmail.com>
2018-12-18 17:02:07 +0100

    Added `<wt-demo>` component.

---
+++ src/app/playground/demo/demo.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'wt-demo',
+    templateUrl: './demo.component.html',
+    styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+
+    cityList = [
+        'Luxembourg',
+        'Lyon'
+    ];
+
+    title = 'training';
+
+    ngOnInit() {
+
+        setInterval(() => this.title += '.', 1000);
+
+    }
+
+    getPictureUri() {
+        const title = encodeURIComponent(this.title);
+        return `https://robohash.org/${title}?set=set4`;
+    }
+
+    canReset() {
+        return this.title.length > 10;
+    }
+
+    reset() {
+        this.title = '';
+    }
+
+}
