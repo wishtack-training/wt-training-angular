a3518da0c5560455550a8c1edbfe9adb473562f4
Younes JAAIDI <yjaaidi@gmail.com>
2018-12-18 14:57:28 +0100

    Added specs.

--- src/app/book/book-store.spec.ts
+++ src/app/book/book-store.spec.ts
@@ -1,11 +1,56 @@
 describe('BookStore', () => {
 
+    let bookStore: BookStore;
+
+    let book1: Book;
+    let book2: Book;
+    let book3: Book;
+
+    beforeEach(() => {
+
+        bookStore = new BookStore();
+
+        book1 = new Book('eXtreme Programming Explained', 'Kent Beck');
+        book2 = new Book('ReWork', 'Jason Fried');
+        book3 = new Book('eXtreme Programming Explained', 'Kent Beck');
+
+    });
+
     xit('should add books', () => {
-        throw new Error('😱 Not implemented yet!');
+
+        const bookListEmpty = bookStore.getBookList();
+
+        expect(bookListEmpty).toEqual([]);
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookList).toEqual([
+            book1,
+            book2,
+            book3
+        ]);
+
     });
 
     xit('should remove books', () => {
-        throw new Error('😱 Not implemented yet!');
+
+        bookStore.addBook(book1);
+        bookStore.addBook(book2);
+        bookStore.addBook(book3);
+
+        bookStore.removeBook(book1);
+
+        const bookList = bookStore.getBookList();
+
+        expect(bookList).toEqual([
+            book2,
+            book3
+        ]);
+
     });
 
 });
