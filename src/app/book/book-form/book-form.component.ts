55694f3c8bc9ebe9feccbd91701a4c1b15e09b90
Younes JAAIDI <yjaaidi@gmail.com>
2019-02-05 17:03:36 +0100

    Added custom isbn validator.

--- src/app/book/book-form/book-form.component.ts
+++ src/app/book/book-form/book-form.component.ts
@@ -1,6 +1,35 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Book } from '../book';
+
+
+export const isIsbnValid = (isbn: string) => {
+
+    const length = isbn.replace(/-/g, '').length;
+
+    return length >= 10 && length <= 13;
+
+};
+
+export const isbnValidator: ValidatorFn = (control) => {
+
+    const isbn = control.value;
+
+    if (isbn == null) {
+        return null;
+    }
+
+    if (isbn.length === 0) {
+        return null;
+    }
+
+    if (!isIsbnValid(isbn)) {
+        return {
+            isbn: true
+        };
+    }
+
+};
 
 @Component({
     selector: 'wt-book-form',
@@ -17,6 +46,9 @@ export class BookFormComponent {
             Validators.minLength(3)
         ]),
         author: new FormControl(),
+        isbn: new FormControl(null, [
+            isbnValidator
+        ]),
         price: new FormControl()
     });
 
--- src/app/book/book.ts
+++ src/app/book/book.ts
@@ -3,11 +3,13 @@ export class Book {
 
     title: string;
     author: string;
+    isbn: string;
     price: number;
 
     constructor(args: Partial<Book> = {}) {
         this.title = args.title;
         this.author = args.author;
+        this.isbn = args.isbn;
         this.price = args.price;
     }
 
