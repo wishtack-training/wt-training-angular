25bd9c655565dda40c4b1b82bba8d1a94586df34
Younes JAAIDI <yjaaidi@gmail.com>
2019-02-27 15:52:11 +0100

    Added empty specs for `Group`.

---
+++ src/app/group/group.spec.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ * (c) 2013-2019 Wishtack
+ *
+ */
+
+describe('Group', () => {
+
+    xit('should add people', () => {
+    });
+
+    xit('should remove people', () => {
+    });
+
+    xit('should get count', () => {
+    });
+
+    xit('should get group food constraints', () => {
+    });
+
+});
