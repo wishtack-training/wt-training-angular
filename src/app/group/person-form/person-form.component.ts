f8fbebccb1f3040abc46a8a3296f02e4e179d44c
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-01 10:00:41 +0100

    Added validators and food constraints selector.

--- src/app/group/person-form/person-form.component.ts
+++ src/app/group/person-form/person-form.component.ts
@@ -10,13 +10,35 @@ export class PersonFormComponent implements OnInit {
 
     personForm = new FormGroup({
         firstName: new FormControl(),
-        lastName: new FormControl()
+        lastName: new FormControl(),
+        foodConstraintList: new FormControl(),
+        // foodConstraints: new FormGroup({
+        //     glutenIntolerant: new FormControl(),
+        //     vegan: new FormControl()
+        // })
     });
 
     ngOnInit() {
+
+        // this.personForm.get('firstName').valueChanges
+        //     .subscribe(firstName => {
+        //
+        //         if (this.personForm.get('lastName') != null) {
+        //             return;
+        //         }
+        //
+        //         if (firstName != null && firstName.length > 0) {
+        //             this.personForm.addControl('lastName', new FormControl(null, [
+        //                 Validators.required
+        //             ]));
+        //         }
+        //     });
+
+
     }
 
     submitPerson() {
         console.log(this.personForm.value);
     }
+
 }
