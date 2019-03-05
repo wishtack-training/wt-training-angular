ecc05f8691c13c0337248fdc6a79086a067effb5
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-05 15:09:32 +0100

    Added empty `SandwichStore` spec.

---
+++ src/app/sandwich/sandwich-store.spec.ts
@@ -0,0 +1,25 @@
+/**
+ *
+ * (c) 2013-2019 Wishtack
+ *
+ */
+
+describe('SandwichStore', () => {
+
+    xit('should add sandwiches', () => {
+
+    });
+
+    xit('should remove sandwiches', () => {
+
+    });
+
+    xit('should search a sandwich by constraints', () => {
+
+    });
+
+    xit('should like a sandwich', () => {
+
+    });
+
+});
