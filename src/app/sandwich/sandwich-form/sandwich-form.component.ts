f9645bc6c6cf4efff9de7e749b6979221484e1a7
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-06 17:09:47 +0100

    Added sandwich constraints validation.

--- src/app/sandwich/sandwich-form/sandwich-form.component.ts
+++ src/app/sandwich/sandwich-form/sandwich-form.component.ts
@@ -1,8 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Sandwich, SandwichConstraint } from '../sandwich';
 
-console.log(SandwichConstraint);
+export const sandwichConstraintsValidator: ValidatorFn = (control) => {
+
+    if (control.value == null) {
+        return null;
+    }
+
+    if (control.value.includes(SandwichConstraint.Vegan)
+        && control.value.includes(SandwichConstraint.Vegetarian)) {
+        return {
+            sandwichConstraints: {
+                value: control.value,
+                message: 'Tu exagères!'
+            }
+        };
+    }
+
+    return null;
+};
 
 @Component({
     selector: 'wt-sandwich-form',
@@ -16,12 +33,11 @@ export class SandwichFormComponent {
     constraintList = Object.values(SandwichConstraint);
 
     sandwichForm = new FormGroup({
-        constraintList: new FormControl(),
+        constraintList: new FormControl(null, sandwichConstraintsValidator),
         name: new FormControl(null, Validators.required),
     });
 
     onSubmit() {
-        console.log(this.sandwichForm.value);
         const sandwich = new Sandwich(this.sandwichForm.value);
         this.sandwichForm.reset();
         this.sandwichSubmit.emit(sandwich);
