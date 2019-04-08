db693578a4fbcd1313ebcbb93f2bb3110cf4a289
Younes JAAIDI <yjaaidi@gmail.com>
2019-04-08 16:44:31 +0200

    🗜 Added empty specs for `CandidateStore`.

---
+++ src/app/candidate/candidate-store.spec.ts
@@ -0,0 +1,18 @@
+describe('CandidateStore', () => {
+
+    xit('should add candidates', () => {
+    });
+
+    xit('should get candidates', () => {
+    });
+
+    xit('should remove candidates', () => {
+    });
+
+    xit('should update candidate', () => {
+    });
+
+    xit('should filter candidates', () => {
+    });
+
+});
