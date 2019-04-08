2a0c2e30df5a01f58d4acf49757b5943d9808583
Younes JAAIDI <yjaaidi@gmail.com>
2019-04-08 16:59:38 +0200

    🗜 Added specs for `CandidateStore`.

--- src/app/candidate/candidate-store.spec.ts
+++ src/app/candidate/candidate-store.spec.ts
@@ -1,20 +1,54 @@
 describe('CandidateStore', () => {
 
+    let candidateStore: CandidateStore;
+    let candidate1: Candidate;
+    let candidate2: Candidate;
+    let candidate3: Candidate;
+
+
+    beforeEach(() => {
+
+        candidateStore = new CandidateStore();
+
+        candidate1 = new Candidate('Foo', 'BAR', [
+            'Angular',
+            'JavaScript'
+        ]);
+        candidate2 = new Candidate('John', 'DOE', [
+            'Angular',
+            'JavaScript',
+            'Python'
+        ]);
+        candidate3 = new Candidate('Tony', 'STARK', [
+            'Kite-surf',
+            'Python'
+        ]);
+
+        candidateStore.addCandidate(candidate1);
+        candidateStore.addCandidate(candidate2);
+        candidateStore.addCandidate(candidate3);
+
+    });
+
     xit('should add candidates', () => {
-        // 1. new CandidateStore
-        // 2. new Candidate x 3 { firstName, lastName, skillList }
-        // 3. store.addCandidate x 3
-        // 4. store.getCandidateList()
-        // 5. check getCandidateList to equal candidate1, 2 & 3
+
+        expect(candidateStore.getCandidateList()).toEqual([
+            candidate1,
+            candidate2,
+            candidate3
+        ]);
+
     });
 
     xit('should remove candidates', () => {
-        // 1. new CandidateStore
-        // 2. new Candidate x 3 { firstName, lastName, skillList }
-        // 3. store.addCandidate x 3
-        // 4. store.removeCandidate number 2
-        // 5. store.getCandidateList()
-        // 6. check getCandidateList to equal candidate1 & 3
+
+        candidateStore.removeCandidate(candidate2);
+
+        expect(candidateStore.getCandidateList()).toEqual([
+            candidate1,
+            candidate3
+        ]);
+
     });
 
     xit('should update candidate', () => {
