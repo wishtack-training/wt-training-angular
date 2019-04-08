4838dae2faf9633155dc05ee595ed12e1b384722
Younes JAAIDI <yjaaidi@gmail.com>
2019-04-08 16:49:01 +0200

    🗜 Added empty specs description for `CandidateStore`.

--- src/app/candidate/candidate-store.spec.ts
+++ src/app/candidate/candidate-store.spec.ts
@@ -1,18 +1,28 @@
 describe('CandidateStore', () => {
 
     xit('should add candidates', () => {
-    });
-
-    xit('should get candidates', () => {
+        // 1. new CandidateStore
+        // 2. new Candidate x 3 { firstName, lastName, skillList }
+        // 3. store.addCandidate x 3
+        // 4. store.getCandidateList()
+        // 5. check getCandidateList to equal candidate1, 2 & 3
     });
 
     xit('should remove candidates', () => {
+        // 1. new CandidateStore
+        // 2. new Candidate x 3 { firstName, lastName, skillList }
+        // 3. store.addCandidate x 3
+        // 4. store.removeCandidate number 2
+        // 5. store.getCandidateList()
+        // 6. check getCandidateList to equal candidate1 & 3
     });
 
     xit('should update candidate', () => {
+        throw new Error('😱 Not implemented yet!');
     });
 
     xit('should filter candidates', () => {
+        throw new Error('😱 Not implemented yet!');
     });
 
 });
