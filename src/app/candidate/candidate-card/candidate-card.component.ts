287650c629e51901ef69df7e4211543a28cb6eb7
Younes JAAIDI <yjaaidi@gmail.com>
2019-04-11 14:58:39 +0200

    Enabled OnPush for `<wt-candidate-card>`.

--- src/app/candidate/candidate-card/candidate-card.component.ts
+++ src/app/candidate/candidate-card/candidate-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Candidate } from '../candidate';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'wt-candidate-card',
     templateUrl: './candidate-card.component.html',
     styleUrls: ['./candidate-card.component.scss']
@@ -13,6 +14,9 @@ export class CandidateCardComponent implements OnInit {
     ngOnInit() {
     }
 
+    /**
+     * @todo this should be in `Candidate.pictureUri`.
+     */
     getPictureUri() {
         const name = encodeURIComponent(this.candidate.firstName);
         return `https://robohash.org/${name}?set=set4`;
