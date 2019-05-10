6f9d0af2f4495c1b2b2e7bf1c07eedecfefa3c60
Younes JAAIDI <yjaaidi@gmail.com>
2019-05-10 20:57:56 +0200

    🗜Cart specs. 🎯 Add sandwiches to cart.

--- src/app/cart.service.spec.ts
+++ src/app/cart.service.spec.ts
@@ -7,6 +7,9 @@ describe('CartService', () => {
     beforeEach(() => TestBed.configureTestingModule({}));
 
     xit('should add sandwich', () => {
+        // add sandwich with a price of 4.3
+        // add another sandwich with a price of 10
+        // check both sandwiches are in the cart.. we might need a getSandwichList method.
         throw new Error('🚧 work in progress!');
     });
 
