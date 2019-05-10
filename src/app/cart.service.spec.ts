adfea266468ae14bc41528dcaab36b3518490c27
Younes JAAIDI <yjaaidi@gmail.com>
2019-05-10 20:55:26 +0200

    🗜Cart specs. 🎯 Add sandwiches to cart.

---
+++ src/app/cart.service.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CartService } from './cart.service';
+
+describe('CartService', () => {
+
+    beforeEach(() => TestBed.configureTestingModule({}));
+
+});
