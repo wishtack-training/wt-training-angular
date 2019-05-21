802c34cc58febd9abdfd0a69e9a0123054e63516
Younes JAAIDI <yjaaidi@gmail.com>
2019-05-21 16:29:06 +0200

    🗜 Added empty cart specs.

---
+++ src/app/cart/cart.spec.ts
@@ -0,0 +1,11 @@
+describe('Cart', () => {
+
+    xit('should add sandwich', () => {
+        throw new Error('🚧 work in progress!');
+    });
+
+    xit('should get total price', () => {
+        throw new Error('🚧 work in progress!');
+    });
+
+});
