61e27f0cf6bd87a47f7cfdb34f5c42a4484328b1
Younes JAAIDI <yjaaidi@gmail.com>
2019-05-21 17:32:29 +0200

    Added Cart service.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,39 +1,41 @@
+import { Cart } from './cart';
+import { Sandwich } from './sandwich';
 
 describe('Cart', () => {
 
-    // let burger: Sandwich;
-    // let butter: Sandwich;
-    // let cart: Cart;
-    //
-    // beforeEach(() => {
-    //
-    //     burger = new Sandwich('Burger', 10);
-    //     butter = new Sandwich('Jambon & Beurre', 5);
-    //
-    //     cart = new Cart();
-    //
-    // });
+    let burger: Sandwich;
+    let butter: Sandwich;
+    let cart: Cart;
+
+    beforeEach(() => {
+
+        burger = new Sandwich('Burger', 10);
+        butter = new Sandwich('Jambon & Beurre', 5);
+
+        cart = new Cart();
+
+    });
 
     xit('should add sandwich', () => {
 
-        // cart.addSandwich(burger);
-        // cart.addSandwich(butter);
-        //
-        // const sandwichList = cart.getSandwichList();
-        //
-        // expect(sandwichList).toEqual([
-        //     burger,
-        //     butter
-        // ]);
+        cart.addSandwich(burger);
+        cart.addSandwich(butter);
+
+        const sandwichList = cart.getSandwichList();
+
+        expect(sandwichList).toEqual([
+            burger,
+            butter
+        ]);
 
     });
 
     xit('should get total price', () => {
 
-        // cart.addSandwich(burger);
-        // cart.addSandwich(butter);
-        //
-        // expect(cart.getTotalPrice()).toEqual(15);
+        cart.addSandwich(burger);
+        cart.addSandwich(butter);
+
+        expect(cart.getTotalPrice()).toEqual(15);
 
     });
 
---
+++ src/app/cart/cart.ts
@@ -0,0 +1,15 @@
+import { Sandwich } from './sandwich';
+
+export class Cart {
+    addSandwich(sandwich: Sandwich) {
+        throw new Error('🚧 work in progress!');
+    }
+
+    getSandwichList(): Sandwich[] {
+        throw new Error('🚧 work in progress!');
+    }
+
+    getTotalPrice(): number {
+        throw new Error('🚧 work in progress!');
+    }
+}
