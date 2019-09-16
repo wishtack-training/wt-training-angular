75063058f9693ab0d52bf6315bc8800853cd4592
Younes JAAIDI <yjaaidi@gmail.com>
2019-09-16 14:45:16 +0200

    Angular CLI 8.3 upgrade.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to training!');
+    expect(page.getTitleText()).toEqual('training app is running!');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('wt-root h1')).getText() as Promise<string>;
+    return element(by.css('wt-root .content span')).getText() as Promise<string>;
   }
 }
