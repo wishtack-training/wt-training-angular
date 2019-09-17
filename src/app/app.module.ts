424b263a2fccb1a1ee481a339bc6824bbde71d41
Younes JAAIDI <yjaaidi@gmail.com>
2019-09-17 15:11:30 +0200

    ✅ Using reactive forms in <wt-demo>.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +16,8 @@ import { DemoComponent } from './demo/demo.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
     bootstrap: [AppComponent]
 })
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { filter } from 'rxjs/operators';
+
+export enum Coolness {
+    cool = 'cool',
+    notCool = 'not-cool'
+}
+
+export interface CityInfo {
+    coolness: Coolness;
+    name: string;
+}
+
+export const postalCodeOrRegion: ValidatorFn = (control) => {
+
+    if (!control.value.postalCode && !control.value.region) {
+        return {
+            postalCodeOrRegion: true
+        };
+    }
+
+    return null;
+};
 
 @Component({
     selector: 'wt-demo',
@@ -7,24 +30,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
 
-    cityList = [
-        'Lyon ☀️',
-        'Paris 🌧',
-        'Rennes'
+    Coolness = Coolness;
+
+    cityInfoForm = new FormGroup({
+        name: new FormControl(null, [
+            Validators.required,
+            Validators.minLength(3)
+        ]),
+        coolness: new FormControl(Coolness.cool),
+        postalCode: new FormControl(),
+        region: new FormControl()
+    }, [
+        postalCodeOrRegion
+    ]);
+
+    cityInfoList: CityInfo[] = [
+        {name: 'Lyon ☀️', coolness: Coolness.cool},
+        {name: 'Paris 🌧', coolness: Coolness.notCool},
+        {name: 'Rennes', coolness: Coolness.cool}
     ];
 
     title = 'Welcome';
-    city: string;
 
     ngOnInit() {
-        setInterval(() => this.title += '.', 200);
+
+        this.cityInfoForm.get('name').valueChanges
+            .pipe(
+                filter(name => name != null),
+                filter(name => name.toLowerCase().includes('paris'))
+            )
+            .subscribe(() => {
+
+                this.cityInfoForm.patchValue({
+                    coolness: Coolness.notCool,
+                    postalCode: '75000',
+                    region: 'Ile de France'
+                });
+
+            });
+
+        // setInterval(() => this.title += '.', 200);
     }
 
     isResetEnabled() {
         return this.title.length > 20;
     }
 
-    addCity() {
-        this.cityList = [...this.cityList, this.city];
+    addCityInfo() {
+        this.cityInfoList = [...this.cityInfoList, this.cityInfoForm.value];
+        this.cityInfoForm.reset();
     }
 }
