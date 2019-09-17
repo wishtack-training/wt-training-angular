5bb1065762d4f75f2ba84a8c5261fb23a5e1658d
Younes JAAIDI <yjaaidi@gmail.com>
2019-09-17 11:58:43 +0200

    ✅ <wt-cart-detail> can add and remove sandwiches.

--- src/app/cart/cart-detail/cart-detail.component.ts
+++ src/app/cart/cart-detail/cart-detail.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Cart } from '../cart';
+import { Sandwich } from '../sandwich';
 
 @Component({
     selector: 'wt-cart-detail',
     templateUrl: './cart-detail.component.html',
     styleUrls: ['./cart-detail.component.scss']
 })
-export class CartDetailComponent implements OnInit {
+export class CartDetailComponent {
 
-    cart = new Cart();
+    private _cart = new Cart();
 
-    constructor() {
+    addSandwich() {
+        const title = prompt('title');
+        const price = parseFloat(prompt('price'));
+        const sandwich = new Sandwich(title, price);
+        this._cart.addSandwich(sandwich);
     }
 
-    ngOnInit() {
-        // this.cart.addSandwich(new )
+    getSandwichList() {
+        return this._cart.getSandwichList();
     }
 
+    removeSandwich(sandwich: Sandwich) {
+        this._cart.removeSandwich(sandwich);
+    }
+
+    addBurger() {
+        this._cart.addSandwich(new Sandwich('Burger', 10));
+    }
 }
