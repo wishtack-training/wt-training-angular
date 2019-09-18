81440e29b235440bd048a14e5be95535442f2e6a
Younes JAAIDI <yjaaidi@gmail.com>
2019-09-18 14:01:05 +0200

    ✅ Using OnPush change detection strategy for <wt-sandwich-form>.

--- src/app/sandwich-preview/sandwich-preview.component.ts
+++ src/app/sandwich-preview/sandwich-preview.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Sandwich } from '../cart/sandwich';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wt-sandwich-preview',
   templateUrl: './sandwich-preview.component.html',
   styleUrls: ['./sandwich-preview.component.scss']
