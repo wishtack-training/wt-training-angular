3dd0e56ca32ce70099f6a00f7ecb3c39c1d2dd13
Younes JAAIDI <yjaaidi@gmail.com>
2019-09-19 10:47:38 +0200

    🚧 Retrieving sandwiches from API, the UGLY WAY!!! Don't do this at home! 🤮

--- playground.ts
+++ playground.ts
@@ -1,16 +1,17 @@
-interface Serializable {
-    serialize(): string;
-}
+import { interval } from 'rxjs';
+import { filter, map, take, tap } from 'rxjs/operators';
 
-class Customer implements Serializable {
-
-    readonly firstName: string;
-    lastName: string;
-
-    serialize() {
-        return this.firstName;
-    }
-
-}
-
-console.log(new Customer());
+interval(100)
+  .pipe(
+    filter(value => value % 3 === 0),
+    map(value => value + 10),
+    tap(value => {
+        if (value === 19) throw new Error('test');
+    }),
+    take(5)
+  )
+  .subscribe({
+      next: data => console.log(data),
+      error: err => console.error('ERROR'),
+      complete: () => console.log('DONE!')
+  });
--- src/app/search/sandwich-search.component.ts
+++ src/app/search/sandwich-search.component.ts
@@ -1,8 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { concat, defer, of } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
+import { Sandwich } from '../cart/sandwich';
 
 export interface ApiSandwich {
   name: string;
+  price: {
+    amount: number
+    currency: string;
+  };
 }
 
 @Component({
@@ -12,14 +20,41 @@ export interface ApiSandwich {
 })
 export class SandwichSearchComponent implements OnInit {
 
+  keywordsControl = new FormControl();
+  sandwichList: Sandwich[];
+
   constructor(private _httpClient: HttpClient) {
   }
 
   ngOnInit() {
-    this._httpClient.get<ApiSandwich[]>('https://sandwich.now.sh/sandwiches')
-      .subscribe(data => {
-        console.log(data);
+
+    /* Forcing valueChanges to start with current value. */
+    const keywords$ = concat(
+      defer(() => of(this.keywordsControl.value)),
+      this.keywordsControl.valueChanges
+    )
+      .pipe(
+        map(keywords => keywords && keywords.toLowerCase()),
+        distinctUntilChanged()
+      );
+
+    keywords$
+      .subscribe(keywords => {
+
+        const params = keywords != null ? {q: keywords} : {};
+
+        this._httpClient.get<ApiSandwich[]>('https://sandwich.now.sh/sandwiches', {
+          params
+        })
+          .subscribe(itemList => {
+            this.sandwichList = itemList.map(item => new Sandwich({
+              title: item.name,
+              price: item.price.amount
+            }));
+          });
+
       });
+
   }
 
 }
