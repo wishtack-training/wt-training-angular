e72591e3dcad23eba2e42439fb373a2dc56012fe
Younes JAAIDI <yjaaidi@gmail.com>
2019-09-19 15:14:46 +0200

    ✅ Angular material setup using `ng add @angular/material`.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,8 @@ import { SandwichSearchComponent } from './sandwich-search/sandwich-search.compo
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   bootstrap: [AppComponent]
 })
