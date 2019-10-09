8131109d066191e11150feb985789818cd549158
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-09 12:06:47 +0200

    test SettingsService

--- src/app/settings/settings.service.spec.ts
+++ src/app/settings/settings.service.spec.ts
@@ -1,12 +1,43 @@
+import { TestBed } from '@angular/core/testing';
+import { SettingsQuery } from './settings.query';
+import { SettingsService } from './settings.service';
+import { SettingsStore } from './settings.store';
+
 describe('SettingsService', () => {
-  xit('🚧 should show toolbar', () => {
-    // create settingsService
-    // check settingsQuery value is toolbar displayed should be false
-    // settingsService.showToolbar()
-    // check settingsQuery value is toolbar displayed should be true
-    throw new Error('🚧 work in progress!');
+  let settingsService: SettingsService;
+  beforeEach(() => (settingsService = TestBed.get(SettingsService)));
+
+  let settingsStore: SettingsStore;
+  beforeEach(() => (settingsStore = TestBed.get(SettingsStore)));
+
+  let settingsQuery: SettingsQuery;
+  beforeEach(() => (settingsQuery = TestBed.get(SettingsQuery)));
+
+  it('should hide toolbar', () => {
+    settingsStore.update({isToolbarDisplayed: true});
+
+    settingsService.hideToolbar();
+
+    expect(settingsQuery.getValue().isToolbarDisplayed).toBe(false);
   });
 
-  xit('🚧 should hide toolbar', () => {
+  it('should show toolbar', () => {
+    settingsStore.update({isToolbarDisplayed: false});
+
+    settingsService.showToolbar();
+
+    expect(settingsQuery.getValue().isToolbarDisplayed).toBe(true);
+  });
+
+  it('should toggle toolbar', () => {
+    settingsStore.update({isToolbarDisplayed: false});
+
+    settingsService.toggleToolbar();
+
+    expect(settingsQuery.getValue().isToolbarDisplayed).toBe(true);
+
+    settingsService.toggleToolbar();
+
+    expect(settingsQuery.getValue().isToolbarDisplayed).toBe(false);
   });
 });
