14351ff5a132958b8437cf2960346ce6397e9e1b
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-09 11:54:09 +0200

    🚧 comment-driven development

---
+++ src/app/settings/settings.service.spec.ts
@@ -0,0 +1,12 @@
+describe('SettingsService', () => {
+  xit('🚧 should show toolbar', () => {
+    // create settingsService
+    // check settingsQuery value is toolbar displayed should be false
+    // settingsService.showToolbar()
+    // check settingsQuery value is toolbar displayed should be true
+    throw new Error('🚧 work in progress!');
+  });
+
+  xit('🚧 should hide toolbar', () => {
+  });
+});
