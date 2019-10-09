7289f8f87027dc6f0e5377016e70b9bdc9ca6bcb
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-09 15:30:50 +0200

    🗜 test <app-login>

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,10 +1,22 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 import { UserService } from '../user/user.service';
 import { LoginComponent, LoginModule } from './login.component';
+
+function getByDataRole(fixture: ComponentFixture<any>, dataRole: string) {
+  return fixture.debugElement.query(
+    By.css(`[data-role="${encodeURIComponent(dataRole)}"]`)
+  );
+}
+
+function submit(form: DebugElement) {
+  return form.triggerEventHandler('submit', {});
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -19,6 +31,7 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   let router: Router;
@@ -35,16 +48,18 @@ describe('LoginComponent', () => {
       email: 'foobar@wishtack.io',
       password: '123456'
     });
-    // @todo click on submit button
-    component.logIn();
 
-    expect(router.navigate).toHaveBeenCalledWith(['/', 'user-profile']);
+    const loginForm = getByDataRole(fixture, 'login-form');
+
+    submit(loginForm);
 
     expect(userService.logIn).toHaveBeenCalledTimes(1);
     expect(userService.logIn).toHaveBeenCalledWith({
       email: 'foobar@wishtack.io',
       password: '123456'
     });
+
+    expect(router.navigate).toHaveBeenCalledWith(['/', 'user-profile']);
   });
 
   xit('🚧 should show error message', () => {});
