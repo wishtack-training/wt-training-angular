03a6de266d762652d60ad98f18b6fd3d1021148f
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-04 10:33:04 +0100

    🚧 add <as-item>

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'as-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
