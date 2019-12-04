96de4e2e3140bd273693a3d3b396f49dbef53e3f
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-04 16:57:35 +0100

    ✅ use <as-item>

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { createItem } from '../cart/item';
+import { Component, Input } from '@angular/core';
+import { createItem, Item } from '../cart/item';
 
 @Component({
   selector: 'as-item',
@@ -8,27 +8,27 @@ import { createItem } from '../cart/item';
 })
 export class ItemComponent {
 
-  item = createItem({title: 'eXtreme Programming Explained', price: 5});
-  private _interval;
+  @Input() item: Item;
+  // private _interval;
 
   canBuy() {
     return this.item.price < 30;
   }
 
-  isMadness() {
-    return this._interval != null;
-  }
-
-  startMadness() {
-    this.stopMadness();
-    this._interval = setInterval(() => {
-      // this.item.price = 50 * Math.random();
-      this.item = createItem({title: 'eXtreme Programming Explained', price: 50 * Math.random()});
-    }, 50);
-  }
-
-  stopMadness() {
-    clearInterval(this._interval);
-    this._interval = null;
-  }
+  // isMadness() {
+  //   return this._interval != null;
+  // }
+  //
+  // startMadness() {
+  //   this.stopMadness();
+  //   this._interval = setInterval(() => {
+  //     // this.item.price = 50 * Math.random();
+  //     this.item = createItem({title: 'eXtreme Programming Explained', price: 50 * Math.random()});
+  //   }, 50);
+  // }
+  //
+  // stopMadness() {
+  //   clearInterval(this._interval);
+  //   this._interval = null;
+  // }
 }
