4342908157994f0bbdad5ae5b6bbc646222f9940
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-04 11:22:36 +0100

    ✅ add <as-item>

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { createItem } from '../cart/item';
 
 @Component({
   selector: 'as-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent {
 
-  constructor() {
+  item = createItem('eXtreme Programming Explained', 5);
+  private _interval;
+
+  canBuy() {
+    return this.item.price < 30;
   }
 
-  ngOnInit() {
+  isMadness() {
+    return this._interval != null;
   }
 
+  startMadness() {
+    this.stopMadness();
+    this._interval = setInterval(() => {
+      // this.item.price = 50 * Math.random();
+      this.item = createItem('eXtreme Programming Explained', 50 * Math.random());
+    }, 50);
+  }
+
+  stopMadness() {
+    clearInterval(this._interval);
+    this._interval = null;
+  }
 }
