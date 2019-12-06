34cbd8ee215ff141ba22e8d8cf427b82e8de03ca
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-06 16:55:48 +0100

    ✅ use dynamic links depending on store state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CartQuery } from './cart/cart.query';
 import { Cart } from './cart/cart.service';
 import { Link } from './nav/nav.component';
 
@@ -10,18 +13,21 @@ import { Link } from './nav/nav.component';
 export class AppComponent {
   title = 'training';
 
-  links: Link[] = [
-    {
-      route: '/book/search',
-      name: 'Search'
-    },
-    {
-      route: '/cart',
-      name: 'Cart'
-    }
-  ];
+  links$: Observable<Link[]> = combineLatest([this._cartQuery.isEmpty$, this._cartQuery.totalPrice$])
+    .pipe(map(([isEmpty, totalPrice]) => {
+      return [
+        {
+          route: '/book/search',
+          name: 'Search'
+        },
+        ...isEmpty ? [] : [{
+          route: '/cart',
+          name: `Cart (${totalPrice})`
+        }]
+      ];
+    }));
 
-  constructor(private _cart: Cart) {
+  constructor(private _cart: Cart, private _cartQuery: CartQuery) {
   }
 
 }
