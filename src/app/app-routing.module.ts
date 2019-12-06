291c2276861846b8e1572fc4c5ecd16163804d54
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-06 14:38:05 +0100

    ✅ customize angular material nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookViewsModule } from './views/book/book-views.module';
+import { CartComponent } from './views/cart/cart/cart.component';
 
 const routes: Routes = [
   {
--- src/app/views/book/book-views.module.ts
+++ src/app/views/book/book-views.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CartComponent } from '../cart/cart/cart.component';
 import { BookSearchComponent } from './book-search/book-search.component';
 
 export const bookRoutes = [
