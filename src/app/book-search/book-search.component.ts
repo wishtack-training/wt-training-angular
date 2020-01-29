766869260e432b8db487795de5eaf6ccd1f23a06
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-29 17:19:53 +0100

    🚧 add book search

--- src/app/book-search/book-search.component.ts
+++ src/app/book-search/book-search.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { removeUndefinedFields } from '../../lib/remove-undefined-fields';
+import { Book, createBook } from '../cart/cart';
 
 export enum Order {
   Newest = 'newest',
@@ -13,13 +14,35 @@ export enum Language {
   French = 'fr'
 }
 
+export interface VolumeItem {
+  id: string;
+  saleInfo: {
+    listPrice: {
+      amount: number;
+      currencyCode: string;
+    };
+  };
+  volumeInfo: {
+    imageLinks: {
+      thumbnail: string;
+    };
+    title: string;
+  };
+}
+
+export interface GoogleListResponse<TItem> {
+  totalItems: number;
+  items: TItem[];
+}
+
+export type VolumeListResponse = GoogleListResponse<VolumeItem>;
+
 @Component({
   selector: 'mc-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.css']
 })
 export class BookSearchComponent implements OnInit {
-
   Order = Order;
   Language = Language;
 
@@ -32,6 +55,8 @@ export class BookSearchComponent implements OnInit {
     order: new FormControl(Order.Newest)
   });
 
+  books: Book[] = [];
+
   constructor(private _httpClient: HttpClient) {
   }
 
@@ -43,15 +68,24 @@ export class BookSearchComponent implements OnInit {
 
   search() {
     const {keywords} = this.searchForm.value;
-    const result$ = this._httpClient.get('https://www.googleapis.com/books/v1/volumes', {
-      params: removeUndefinedFields({
-        q: keywords
-      })
-    });
+    const result$ = this._httpClient.get<VolumeListResponse>(
+      'https://www.googleapis.com/books/v1/volumes',
+      {
+        params: removeUndefinedFields({
+          q: keywords
+        })
+      }
+    );
 
     result$.subscribe(data => {
+      this.books = data.items.map(item => {
+        return createBook({
+          title: item.volumeInfo.title,
+          pictureUri: item.volumeInfo.imageLinks?.thumbnail,
+          price: item.saleInfo.listPrice?.amount
+        });
+      });
     });
 
-    // @todo {keywords, orderBy, langRestrict}
   }
 }
