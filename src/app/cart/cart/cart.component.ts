af0a91b2a355f93b05e3e0e630c099a1093936e6
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-29 14:25:53 +0100

    ✅ remove add button

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Cart, createBook } from '../cart';
+import { Cart } from '../cart';
 
 @Component({
   selector: 'mc-cart',
@@ -9,26 +9,10 @@ import { Cart, createBook } from '../cart';
 export class CartComponent {
 
   constructor(private _cart: Cart) {
-    this._cart.addBook(createBook({
-      title: 'eXtreme Programming Explained',
-      pictureUri: 'https://images-na.ssl-images-amazon.com/images/I/416Y8MS65TL._SX258_BO1,204,203,200_.jpg',
-      price: 20
-    }));
-    this._cart.addBook(createBook({
-      title: 'Rework',
-      pictureUri: 'https://images-na.ssl-images-amazon.com/images/I/71SqvmjaM3L.jpg',
-      price: 30
-    }));
   }
 
   getBookList() {
     return this._cart.getBookList();
   }
 
-  addBook() {
-    this._cart.addBook(createBook({
-      title: 'test',
-      price: 123
-    }));
-  }
 }
