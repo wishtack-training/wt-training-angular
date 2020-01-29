c7b27bcc7b89ddea9354ca411a48e18b41a33512
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-29 15:42:14 +0100

    🚧 add book search

--- src/app/book-search/book-search.component.ts
+++ src/app/book-search/book-search.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.css']
 })
-export class BookSearchComponent {
-
+export class BookSearchComponent implements OnInit {
   searchForm = new FormGroup({
-    keywords: new FormControl()
+    keywords: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3)
+    ])
   });
 
+  ngOnInit() {
+    this.searchForm.valueChanges.subscribe(data => {
+      console.log(data);
+    });
+  }
+
   search() {
+    console.log(this.searchForm.valid);
     console.log(this.searchForm.value);
     // @todo {keywords, orderBy, langRestrict}
   }
