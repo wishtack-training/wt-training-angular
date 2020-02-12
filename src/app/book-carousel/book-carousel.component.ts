676dfaf218e7fe356050cdc0bfa5f1f9da9a8926
Younes Jaaidi <yjaaidi@gmail.com>
2020-02-12 11:56:19 +0100

    ✅ add carousel

---
+++ src/app/book-carousel/book-carousel.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../cart/cart';
+
+@Component({
+  selector: 'ag-book-carousel',
+  templateUrl: './book-carousel.component.html',
+  styleUrls: ['./book-carousel.component.css']
+})
+export class BookCarouselComponent implements OnInit {
+
+  selectedBook: Book;
+
+  books: Book[] = [
+    {
+      title: 'Rework',
+      price: 20
+    },
+    {
+      title: 'eXtreme Programming Explained',
+      price: 30
+    },
+    {
+      title: 'Lean Startup',
+      price: 25
+    }
+  ];
+
+  ngOnInit(): void {
+    this.selectedBook = this.books[0];
+  }
+
+  goToNext() {
+    const index = this._getSelectedBookIndex();
+    this.selectedBook = this.books[index + 1];
+  }
+
+  goToPrevious() {
+    const index = this._getSelectedBookIndex();
+    this.selectedBook = this.books[index - 1];
+  }
+
+  isFirst() {
+    return this._getSelectedBookIndex() === 0;
+  }
+
+  isLast() {
+    return this._getSelectedBookIndex() === this.books.length - 1;
+  }
+
+  selectBook(book: Book) {
+    this.selectedBook = book;
+  }
+
+  private _getSelectedBookIndex() {
+    return this.books.indexOf(this.selectedBook);
+  }
+}
